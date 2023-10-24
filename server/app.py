a1feaffe3e2813b9bbccf672975076eede0f0223
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-10-24 17:29:32 -0400

    in-progress backend setup
    drafting models and backend API routes

--- server/app.py
+++ server/app.py
@@ -3,15 +3,190 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
-
+# Add your model imports 
+from models import User, Fave, Medication, Condition, Treatment
+from datetime import datetime, date
 
 # Views go here!
+
+#POST to login
+class Login(Resource):
+    def post(self):
+        data = request.json
+        print(data["email"])
+        user = User.query.filter_by(email=data["email"]).first()
+        print(user)
+        if user is None:
+            return make_response({"error": "user not found"}, 404)
+        elif user.password != data["password"]:
+            return make_response({"error": "incorrect password"}, 403)
+        else:
+            return make_response({"id": user.id}, 200)
+
+api.add_resource(Login, '/login')
+
+#POST to logout 
+# class Logout(Resource):
+#     def post(self):
+#         data = request.json
+
+
+# api.add_resource(Logout, '/logout')
+
+#POST a new account for user
+class Users(Resource):
+    def post(self):
+        data = request.json
+        birthday = data["birthday"].split("-")
+        fixed_birthday = date(int(birthday[0]), int(
+            birthday[1]), int(birthday[2]))
+        try:
+
+            new_user = User(
+                name=data["name"],
+                email=data["email"],
+                password=data["password"],
+                birthday=fixed_birthday,
+                pronouns = ["pronouns"]
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return make_response(new_user.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(Users, '/users')
+
+#GET, PATCH, and DELETE user accts
+#users by ID stuff (serialize faves into here somehow?)
+class UsersByID(Resource):
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({"error": "No user was found"}, 404)
+
+#what would patching email/password look like here instead?
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+        data = request.json
+        if data["birthday"]:
+            birthday = data["birthday"].split("-")
+            fixed_birthday = date(int(birthday[0]), int(
+                birthday[1]), int(birthday[2]))
+        if user:
+            try:
+                for attr in data:
+                    if attr == "birthday":
+                        setattr(user, attr, fixed_birthday)
+                    else:
+                        setattr(user, attr, data[attr])
+
+                db.session.add(user)
+                db.session.commit()
+
+                return make_response(user.to_dict(), 201)
+            except ValueError:
+                return make_response({"errors": ["validation errors"]}, 400)
+
+        else:
+            return make_response({"error": "No user was found"}, 404)
+
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+            return make_response({"message": "user was successfully deleted"}, 204)
+        else:
+            return make_response({"error": "No user was found"}, 404)
+
+api.add_resource(UsersByID, '/users/<int:id>')
+
+#PATCH and DELETE user faves
+class UserFaves(Resource):
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+        data = request.json
+        #I don't know how to make this into a fave affiliated with a user
+        # is it data["fave"]?
+        if data["birthday"]:
+            birthday = data["birthday"].split("-")
+            fixed_birthday = date(int(birthday[0]), int(
+                birthday[1]), int(birthday[2]))
+        if user:
+            try:
+                for attr in data:
+                    if attr == "birthday":
+                        setattr(user, attr, fixed_birthday)
+                    else:
+                        setattr(user, attr, data[attr])
+
+                db.session.add(user)
+                db.session.commit()
+
+                return make_response(user.to_dict(), 201)
+            except ValueError:
+                return make_response({"errors": ["validation errors"]}, 400)
+
+        else:
+            return make_response({"error": "No user was found"}, 404)
+
+    def delete(self, id):
+        #not sure how to specify the faves of the user? is it just user.faves?
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+            return make_response({"message": "user was successfully deleted"}, 204)
+        else:
+            return make_response({"error": "No user was found"}, 404)
+
+
+api.add_resource(UserFaves, '/users/<int:id>/faves')
+
+#GET medication by ID
+class MedicationsById(Resource):
+    def get(self, id):
+        medication = Medication.query.filter_by(id=id).first()
+        if medication:
+            return make_response(medication.to_dict(), 200)
+        else:
+            return make_response({"error": "No medication was found"},404)
+        
+api.add_resource(MedicationsById, '/medications/<int:id>')
+
+#GET for all conditions
+class Conditions(Resource):
+    def get(self):
+        conditions = [condition.to_dict(only=('name', 'id',))
+                     for condition in Condition.query.all()]
+        return make_response(conditions, 200)
+        
+api.add_resource(Conditions, '/conditions')
+
+#GET condition by ID
+class ConditionsById(Resource):
+    def get(self, id):
+        condition = Condition.query.filter_by(id=id).first()
+        if condition:
+            return make_response(condition.to_dict(), 200)
+        else:
+            return make_response({"error": "No condition was found"},404)
+        
+api.add_resource(ConditionsById, '/conditions/<int:id>')
+
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -1,6 +1,150 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from datetime import *
 
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    birthday = db.Column(db.Date)
+    pronouns = db.Column(db.String)
+    #do I need this?
+
+    #relationships with faves
+    faves = db.relationship(
+        "Fave", backref="user", cascade="all, delete-orphan")
+    
+    #serializing so we don't see other faves' associated users
+    serialize_rules =("-faves.user",)
+
+    #validations for name, password, email, birthday
+    @validates('name')
+    def validate_name(self, key, name):
+        if name and len(name) >=1:
+            return name
+        else:
+            raise ValueError("Must have valid name")
+        
+    
+    @validates('password')
+    def validate_password(self, key, password):
+        if password and len(password) >=9:
+            return password
+        else:
+            raise ValueError("Must have valid password")
+        
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if email and '@' in email:
+            return email
+        else:
+            raise ValueError("Must have valid email attribute")
+        
+    
+    @validates('birthday')
+    def validate_birthday(self, key, birthday):
+        today = datetime.now().date()
+        difference= today - birthday
+        age_in_days=difference.days
+        if birthday and age_in_days >= 6570:
+            return birthday
+        else:
+            raise ValueError("Must have valid birthday attribute")
+        
+
+#faves as the intermediate between med and user
+class Fave(db.Model, SerializerMixin):
+    __table_name__ = 'faves'
+
+    id = db.Column(db.Integer, primary_key=True)
+    faved_on = db.Column(db.Date)
+    unfaved_on = db.Column(db.Date)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    medication_id = db.Column(db.Integer, db.ForeignKey('medication.id'))
+
+    #serialize rules
+    serialize_rules = ("-user.faves", "-medication.faves")
+
+
+#medication model 
+
+class Medication(db.Model, SerializerMixin):
+    __tablename__ = 'medications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name_generic = db.Column(db.String)
+    name_brand = db.Column(db.String)
+    #maybe more stuff, like a description, depending on what is in the APIs
+
+    #adding a relationship to faves and treatments
+    faves = db.relationship(
+        "Fave", backref="medication", cascade="all, delete-orphan")
+    
+    #serializing other users' medications (others needed here?)
+    serialize_rules = ("-users.medication",)
+    
+    treatments = db.relationship(
+        "Treatment", backref="medication", cascade="all, delete-orphan")
+    
+    #serializing out other treatments' medication (others needed here?)
+    serialize_rules = ("-treatments.medication",)
+
+#validate for generic and name brand attributes
+    @validates('name_generic')
+    def validate_name_generic(self, key, name_generic):
+        if name_generic and len(name_generic) >=1:
+            return name_generic
+        else:
+            raise ValueError("Must have valid generic name")
+
+    @validates('name_brand')
+    def validate_name_brand(self, key, name_brand):
+        if name_brand and len(name_brand) >=1:
+            return name_brand
+        else:
+            raise ValueError("Must have valid brand name")
+        
+
+class Treatment(db.Model, SerializerMixin):
+    __tablename__ = 'treatments'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    condition_id = db.Column(db.Integer, db.ForeignKey("conditions.id"))
+    medication_id = db.Column(db.Integer, db.ForeignKey("medications.id"))
+
+    #dosage_id = db.Column(db.Integer, db.ForeignKey("dosage.id"))
+
+    #I think this serializes logically?
+    serialize_rules = ("-medication.treatments","-conditions.treatments")
+
+
+class Condition(db.Model, SerializerMixin):
+    __table_name__ = "conditions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description= db.Column(db.String)
+
+    #relationship with treatment
+    treatments = db.relationship(
+        "Treatment", backref="condition", cascade="all, delete-orphan")
+    
+    #do I need a validation for this?
+    @validates('name')
+    def validate_name(self, key, name):
+        if name and len(name) >=1:
+            return name
+        else:
+            raise ValueError("Must have valid name")
+        
