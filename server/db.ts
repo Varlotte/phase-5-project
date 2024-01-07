721ca1f0919606852949bad81ab32f32db5fe830
Charlotte Bush <clangbush@gmail.com>
2024-01-07 16:46:19 -0500

    fixing issues caused by previous commit
    fixes buggy typescript config

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import { createValidationMiddleware } from "prisma-validation-middleware";
-import { object, string, nonempty, date, size, pattern, assert, mask, Struct, refine } from "superstruct";
+import {
+  object,
+  string,
+  nonempty,
+  date,
+  size,
+  pattern,
+  assert,
+  mask,
+  Struct,
+  refine,
+} from "superstruct";
 import { differenceInYears } from "date-fns";
 
-const client = new PrismaClient()
+const client = new PrismaClient();
 
 function validate<T extends Struct<any, any>>(struct: T) {
   return (data: any) => assert<T, any>(mask(data, struct), struct);
@@ -17,30 +28,30 @@ function isAdult(input: Date) {
 
 // validate our data based on business logic
 client.$use(
-    createValidationMiddleware({
-      User: validate(
-        object({
-          // user fields
-          name: nonempty(string()),
-          password: size(string(), 12, Infinity),
-          email: pattern(string(), /@/),
-          birthday: refine(date(), 'adult', isAdult),
-        })
-      ),
-      Medication: validate(
-        object({
-          //medication fields
-          nameBrand: nonempty(string()),
-          nameGeneric: nonempty(string())
-        })
-      ),
-      Condition: validate(
-        object({
-          name: nonempty(string()),
-        })
-      )
-    })
-  );
+  createValidationMiddleware({
+    User: validate(
+      object({
+        // user fields
+        name: nonempty(string()),
+        password: size(string(), 12, Infinity),
+        email: pattern(string(), /@/),
+        birthday: refine(date(), "adult", isAdult),
+      })
+    ),
+    Medication: validate(
+      object({
+        //medication fields
+        nameBrand: nonempty(string()),
+        nameGeneric: nonempty(string()),
+      })
+    ),
+    Condition: validate(
+      object({
+        name: nonempty(string()),
+      })
+    ),
+  })
+);
 
 export default client;
 
