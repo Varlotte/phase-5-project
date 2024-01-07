4f51dc8b7cbd925a8336ccbb50c85c3c511c91a6
Charlotte Bush <clangbush@gmail.com>
2024-01-07 16:28:48 -0500

    Initial backend API routing infrastructure
    -sets up what routes we need based on old backend routing
    -establishes which ones will need auth to delineate task difficulty

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,21 @@ app.use(express.static(join(__dirname, "..", "client", "build")));
 
 // api routes TBD
 
+//these will all need auth:
+//POST /login
+//POST /logout
+//POST /users (create new user)
+//GET, PATCH, and DELETE /users/id
+//POST and DELETE user faves, protected to only add faves to your own account
+// DELETE fave but not if its not your account
+
+//no auth needed:
+//GET medications by id
+//GET medication by brand and generic name for search
+
+//GET for all conditions
+//GET condition by ID for the tinder UI
+
 // for all other routes, return to the index so the SPA routing can handle it
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "..", "client", "build", "index.html"));
