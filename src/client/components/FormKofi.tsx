c84df93eedea1c83eaee2606d8c2b6249c4bda87
Varlotte <32116877+Varlotte@users.noreply.github.com>
2024-02-20 17:38:50 -0500

    Adds bug/feature form and ko-fi link compartment to bottom of app (#9)
    * Building out the feature/bug and ko-fi component
    
    -builds component with placeholder text
    -adds it to app
    -styles it in Chakra
    -links to form and kofi tbd
    
    * Updates Favicon
    
    No more react favicon, yay!
    
    * Adds new ko-fi link
    
    * Adds request form link to component
    
    * cleans up favicon file naming

---
+++ src/client/components/FormKofi.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  Box,
+  Link as ChakraLink,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
+import Link from '../components/Link';
+
+export default function FormKofi() {
+  return (
+    <Box bg="#E6FFFA" w="100%" p={7} color="white" marginTop={5}>
+      <Stack spacing={1}>
+        <Text fontSize="xs" align="center" color="black">
+          This tool was made by{' '}
+          <Link href="https://femmefaust.us/dev">Charlotte Bush</Link> and{' '}
+          <Link href="https://nelson.codes"> Nelson Pecora</Link>. If you like
+          it, please consider throwing us a few bucks{' '}
+          <Link href="https://ko-fi.com/charlottebush">here.</Link> Report bugs
+          and request features{' '}
+          <Link href="https://forms.gle/nAUdeLSdzhXbSHN88">here!</Link> Thank
+          you!
+        </Text>
+      </Stack>
+    </Box>
+  );
+}
